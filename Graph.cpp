39863c78e914da7ae81700420030790fa7742cfd
asher-j1 <5542898+AshIndigo@users.noreply.github.com>
2023-04-03 17:19:57 -0400

    Don't mind this, wanted to test something

--- Graph.cpp
+++ Graph.cpp
@@ -2,7 +2,7 @@
 #include "Graph.h"
 
 template<class D, class K>
-Graph<D, K>::Graph(vector<K> keys, vector<D> data, vector<vector<K>> edges) { // List in List for the adjs/edges
+Graph<D, K>::Graph(vector<K> keys, vector<D> data, vector<vector<K>> edges) {
     // Check PDF for new version
     /*
      * vector<string> keys ={"R", "V", "S", "T", "U", "Y", "W", "X"}; // 3
